149cfb649d943565e2a0663831dc9d2b3f0b6b2f
lewiswolf <lewiswolstanholme@gmail.com>
2024-01-29 19:29:27 +0000

    me and rodrigo uploaded our videos

--- src/config.ts
+++ src/config.ts
@@ -4,18 +4,28 @@ const config: SubmissionJSON[] = [
 			name: 'Julia Set X Barrell Jones',
 			links: [{ href: 'https://linktr.ee/julia___set', type: 'website' }],
 		},
+		video: ['ywyDa7hx5ec'],
 	},
 	{
 		author: {
-			name: 'Lewis Wolstanholme',
+			name: 'Lewis Wolstanholme X Ciarán Corr',
 			links: [{ href: 'https://lewiswolstanholme.co.uk', type: 'website' }],
 		},
+		audio: [],
+	},
+	{
+		author: {
+			name: 'Napoleon Skywalker',
+			links: [],
+		},
+		video: [],
 	},
 	{
 		author: {
 			name: 'Rodrigo Constanzo',
 			links: [{ href: 'https://rodrigoconstanzo.com', type: 'website' }],
 		},
+		video: ['4XrQqV744z8', '511Se3g2A2w'],
 	},
 ] as const
 
